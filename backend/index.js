0ee3d1c695edd90500d9460e0cbec011b2270bac
Ubuntu <ubuntu@ip-10-0-0-95.ap-south-1.compute.internal>
2025-12-28 05:33:02 +0000

    Initial realtime chat app with Docker Compose

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("message", (msg) => {
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(5001, "0.0.0.0", () => {
+  console.log("Backend running on port 5001");
+});
+
